9921da57ba22cd2240b48b2a28500501029e6546
Gamaley <gamaley@me.com>
2015-10-26 14:50:35 +0200

    Initial Commit

---
+++ Cat.Meow.Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cat.Meow.Test
+//
+//  Created by Vladyslav on 26.10.15.
+//  Copyright © 2015 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
